d31d76d5b5242f02d765bf3bc6820bc594289f20
Alexey KHokhlow <lomtik25@gmail.com>
2021-02-22 13:40:33 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n
+    }
+    let stringInput = n.toString()
+    let result = ""
+    for (let i = 0; i < stringInput.length; i++) {
+        result = stringInput[i] + result
+    }
+
+    return +result
 }
